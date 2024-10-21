9ec30ba3f919d6588ed77bfc390e2004585db677
Juan-Cumplido <zs22020936@estudiantes.uv.mx>
2024-10-20 19:36:48 -0600

    Creacion de las ventans de amistades, solicitudes y añadir

--- HiveGameClient/HiveGameWPFApp/MainWindow.xaml.cs
+++ HiveGameClient/HiveGameWPFApp/MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace HiveGameWPFApp
 
         private void IntroVideo_MediaEnded(object sender, RoutedEventArgs e)
         {
-            NavigationFrame.Navigate(new Views.EditCredentialsView());
+            NavigationFrame.Navigate(new Views.FriendsListView());
         }
 
         private void NavigationFrame_Navigating(object sender, NavigatingCancelEventArgs e)
