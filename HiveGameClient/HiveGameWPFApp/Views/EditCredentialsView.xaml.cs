3fb8faaf238511957329ea02ccf18583b3d817d2
Juan-Cumplido <zs22020936@estudiantes.uv.mx>
2024-10-26 15:48:27 -0600

    Se creo la ventana de de las estadistica del jugador con la funcionalidad requerida

--- HiveGameClient/HiveGameWPFApp/Views/EditCredentialsView.xaml.cs
+++ HiveGameClient/HiveGameWPFApp/Views/EditCredentialsView.xaml.cs
@@ -7,7 +7,6 @@ using System.Linq;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web.UI.WebControls;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -24,7 +23,7 @@ namespace HiveGameWPFApp.Views
     public partial class EditCredentialsView : Page
     {
         private DispatcherTimer timer;
-        private int timeLeft = 60; 
+        private int timeLeft = 60;
 
         public EditCredentialsView()
         {
@@ -38,12 +37,6 @@ namespace HiveGameWPFApp.Views
         {
             lbl_Email.Content = UserProfileSingleton.email;
             txt_Email.Text = UserProfileSingleton.email;
-        }
-
-        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-            MainMenu mainMenu = new MainMenu();
-            this.NavigationService.Navigate(mainMenu);
         }
         private void StartTimer()
         {
@@ -66,7 +59,7 @@ namespace HiveGameWPFApp.Views
         }
         private void FocusFirstTextBox()
         {
-            TextBox firstTextBox = (TextBox)brd_BubbleOne.Child; 
+            TextBox firstTextBox = (TextBox)brd_BubbleOne.Child;
             firstTextBox.Focus();
         }
 
@@ -87,7 +80,7 @@ namespace HiveGameWPFApp.Views
                 ValidateCode();
             }
         }
-       
+
 
         private void TextBox_GotFocus(object sender, RoutedEventArgs e)
         {
@@ -165,7 +158,7 @@ namespace HiveGameWPFApp.Views
         {
             LoggerManager logger = new LoggerManager(this.GetType());
             HiveProxy.EmailVerificationManagerClient emailVerificationManager = new HiveProxy.EmailVerificationManagerClient();
-            bool isValid = false;  
+            bool isValid = false;
             try
             {
                 UserVerificator verificationUser = new UserVerificator();
@@ -193,7 +186,7 @@ namespace HiveGameWPFApp.Views
 
         private void ShowNewPasswordSection()
         {
-            lbl_ErrorMessage.Visibility= Visibility.Collapsed;
+            lbl_ErrorMessage.Visibility = Visibility.Collapsed;
             grd_VerificationPanel.Visibility = Visibility.Collapsed;
             grd_EditionPanel.Visibility = Visibility.Visible;
         }
@@ -240,7 +233,7 @@ namespace HiveGameWPFApp.Views
             {
                 brd_ConfirmPassword.BorderBrush = Brushes.Red;
             }
-            return emailVerification&&passwordVerificaton&&passwordConfirmVerification;
+            return emailVerification && passwordVerificaton && passwordConfirmVerification;
         }
 
         private bool ValidateSamePasswords()
@@ -265,7 +258,8 @@ namespace HiveGameWPFApp.Views
                 {
                     DialogManager.ShowErrorMessageAlert(Properties.Resources.dialogEmailAlreadyRegistered);
                     resultVerification = false;
-                }else if(resultVerificationCredentials== Constants.NO_DATA_MATCHES)
+                }
+                else if (resultVerificationCredentials == Constants.NO_DATA_MATCHES)
                 {
                     resultVerification = true;
                 }
@@ -313,8 +307,8 @@ namespace HiveGameWPFApp.Views
                     email = txt_Email.Text,
                     password = passwordEncripted
                 };
-                int updateResult = userManagerClient.UpdateLoginCredentials(oldAccesAccount,newAccesAccount);
-                if(updateResult == Constants.SUCCES_OPERATION)
+                int updateResult = userManagerClient.UpdateLoginCredentials(oldAccesAccount, newAccesAccount);
+                if (updateResult == Constants.SUCCES_OPERATION)
                 {
                     DialogManager.ShowSuccessMessageAlert(Properties.Resources.dialogUpdatedData);
                     ReturnToMainWindow();
@@ -348,7 +342,7 @@ namespace HiveGameWPFApp.Views
             try
             {
                 int resultEmailSend = emailVerificationManager.SendVerificationEmail(UserProfileSingleton.email);
-                if(resultEmailSend == Constants.SUCCES_OPERATION)
+                if (resultEmailSend == Constants.SUCCES_OPERATION)
                 {
                     DialogManager.ShowSuccessMessageAlert(Properties.Resources.dialogEmailVerificationMessage);
                 }
@@ -373,7 +367,7 @@ namespace HiveGameWPFApp.Views
                 DialogManager.ShowErrorMessageAlert(Properties.Resources.dialogTimeOutException);
             }
         }
-        
+
 
         private void BtnCancelChange_Click(object sender, RoutedEventArgs e)
         {
@@ -382,6 +376,17 @@ namespace HiveGameWPFApp.Views
         }
 
         public void ReturnToMainWindow()
+        {
+            MainMenu mainMenu = new MainMenu();
+            this.NavigationService.Navigate(mainMenu);
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            GoToLoginView();
+        }
+
+        private void GoToLoginView()
         {
             MainMenu mainMenu = new MainMenu();
             this.NavigationService.Navigate(mainMenu);
